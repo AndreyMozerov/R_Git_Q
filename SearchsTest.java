6f413fb0118e8a95b0be9fbd86b1f9dc008307c9
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-06-17 17:23:57 +0300

    R_G_first commit

---
+++ SearchsTest.java
@@ -0,0 +1,12 @@
+
+
+public class SearchsTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+}
+
