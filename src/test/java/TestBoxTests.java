7444d70a475e0ee9f0a011324da74e8236c95d21
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-06-18 19:21:27 +0300

    Demoqa2 commit

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.open;
 public class TestBoxTests {
+
+    @Test
+    void fillFormTests(){
+       open("https://demoqa.com/text-box");
+    }
 }
