89c223601aa15aac2dc352b56fbea73aa4386141
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-06-18 18:21:57 +0300

    Demoqa1 commit

---
+++ src/test/java/JunitExamples.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+
+public class JunitExamples {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Zero Test");
+    }
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Each Test");
+    }
+
+
+    @Test
+    void firstTest() {
+        System.out.println("    First Test");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("     Second Test");
+    }
+
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("End Test");
+    }
+    @AfterEach
+    void afterEach(){
+        System.out.println("Eachend Test");
+    }
+}
