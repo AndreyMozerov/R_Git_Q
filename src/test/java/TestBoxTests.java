85c8ecd66706a408109c0e345d1ae3cf4fd796c4
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-06-18 19:45:01 +0300

    Demoqa3 commit

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -1,9 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+
 public class TestBoxTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize="1920x1080";
+        Configuration.baseUrl="https://demoqa.com";
+    }
 
     @Test
     void fillFormTests(){
-       open("https://demoqa.com/text-box");
+       open("/text-box");
+        $("#=userName").setValue("Andrey Mozerov");
+        $("#=userEmail").setValue("andreymoz@mail.ru");
+        $("#=currentAddress").setValue("Moscow");
+        $("#=permanentAddress").setValue("Vladimir street");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Andrey Mozerov"), text("andreymoz@mail.ru"),
+                text("Moscow"), text("Vladimir street"));
+
+
     }
 }
